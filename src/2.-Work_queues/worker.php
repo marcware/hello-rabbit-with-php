8443e4aa05ef548115da88a5e54998515a8dde1f
Marcos Palacios <m.palacios@geekshubs.com>
2020-05-01 11:50:25 +0200

    Exercice 2 work in dir

--- src/2.-Work_queues/worker.php
+++ src/2.-Work_queues/worker.php
@@ -1,10 +1,10 @@
 <?php
 
-require_once __DIR__ . '../vendor/autoload.php';
+require_once __DIR__ . '../../vendor/autoload.php';
 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 
-$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$connection = new AMQPStreamConnection('rabbitmq', 5672, 'guest', 'guest');
 $channel = $connection->channel();
 
 $channel->queue_declare('task_queue', false, true, false, false);
